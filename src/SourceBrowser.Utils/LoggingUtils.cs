7879b0dab5c193515837f45a7541831fd822f7ea
Amadeus Wieczorek <git@amadeusw.com>
2015-01-15 13:26:44 -0800

    Adds project with RequestId and Logging utils

---
+++ src/SourceBrowser.Utils/LoggingUtils.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SourceBrowser.Utils
+{
+    public class LoggingUtils
+    {
+        private const string CATEGORY_ERROR = "Error";
+        private const string CATEGORY_WARNING = "Warning";
+        private const string CATEGORY_INFO = "Info";
+
+        /// <summary>
+        /// Logs a message in the Error category
+        /// </summary>
+        /// <param name="message">Message to log</param>
+        /// <param name="memberName">(Automatically populated)</param>
+        /// <param name="sourceFilePath">(Automatically populated)</param>
+        /// <param name="sourceLineNumber">(Automatically populated)</param>
+        public static void Error(string message,
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
+        {
+            string requestId = RequestUtils.GetRequestId().ToString();
+            Trace.WriteLine(String.Format("[{0}, {1}] at {2}:{3} ({4}); {5}", DateTime.UtcNow, requestId, sourceFilePath, sourceLineNumber, memberName, message), CATEGORY_ERROR);
+        }
+
+        /// <summary>
+        /// Logs a message in the Error category and includes the exception information.
+        /// </summary>
+        /// <param name="message">Message to log</param>
+        /// <param name="exception">Exception information to attach</param>
+        /// <param name="memberName">(Automatically populated)</param>
+        /// <param name="sourceFilePath">(Automatically populated)</param>
+        /// <param name="sourceLineNumber">(Automatically populated)</param>
+        public static void Error(string message,
+            Exception exception,
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
+        {
+            string requestId = RequestUtils.GetRequestId().ToString();
+            Trace.WriteLine(String.Format("[{0}, {1}] at {2}:{3} ({4}); {5} \n {6}", DateTime.UtcNow, requestId, sourceFilePath, sourceLineNumber, memberName, message, exception.ToString()), CATEGORY_ERROR);
+            System.Web.HttpContext.Current.Trace.Write(String.Format("xxx [{0}, {1}] at {2}:{3} ({4}); {5} \n {6}", DateTime.UtcNow, requestId, sourceFilePath, sourceLineNumber, memberName, message, exception.ToString()), CATEGORY_ERROR);
+        }
+
+        /// <summary>
+        /// Logs a message in the Warning category
+        /// </summary>
+        /// <param name="message">Message to log</param>
+        /// <param name="memberName">(Automatically populated)</param>
+        /// <param name="sourceFilePath">(Automatically populated)</param>
+        /// <param name="sourceLineNumber">(Automatically populated)</param>
+        public static void Warning(string message,
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
+        {
+            string requestId = RequestUtils.GetRequestId().ToString();
+            Trace.WriteLine(String.Format("[{0}, {1}] at {2}:{3} ({4}); {5}", DateTime.UtcNow, requestId, sourceFilePath, sourceLineNumber, memberName, message), CATEGORY_WARNING);
+        }
+
+        /// <summary>
+        /// Logs a message in the Info category
+        /// </summary>
+        /// <param name="message">Message to log</param>
+        /// <param name="memberName">(Automatically populated)</param>
+        /// <param name="sourceFilePath">(Automatically populated)</param>
+        /// <param name="sourceLineNumber">(Automatically populated)</param>
+        public static void Info(string message,
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
+        {
+            string requestId = RequestUtils.GetRequestId().ToString();
+            Trace.WriteLine(String.Format("[{0}, {1}] at {2}:{3} ({4}); {5}", DateTime.UtcNow, requestId, sourceFilePath, sourceLineNumber, memberName, message), CATEGORY_INFO);
+            System.Web.HttpContext.Current.Trace.Write(String.Format("xxx [{0}, {1}] at {2}:{3} ({4}); {5}", DateTime.UtcNow, requestId, sourceFilePath, sourceLineNumber, memberName, message), CATEGORY_INFO);
+        }
+    }
+}
---
+++ src/SourceBrowser.Utils/RequestUtils.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace SourceBrowser.Utils
+{
+    public class RequestUtils
+    {
+        private const string REQUEST_ID_KEY = "RequestId";
+
+        [ThreadStatic]
+        private static Guid _requestId;
+
+        /// <summary>
+        /// Creates a new RequestId and stores it in current HttpContext/Thread
+        /// </summary>
+        public static void CreateRequestId()
+        {
+            Guid requestId = Guid.NewGuid();
+            // TODO: Pick one:
+            System.Web.HttpContext.Current.Items.Add(REQUEST_ID_KEY, requestId);
+            Thread.SetData(Thread.GetNamedDataSlot(REQUEST_ID_KEY), requestId);
+            _requestId = requestId;
+        }
+
+        /// <summary>
+        /// Retrieves a RequestId from current HttpContext/Thread
+        /// </summary>
+        /// <returns>Guid associated with current HttpContext/Thread, or Guid.Empty if no Guid was found.</returns>
+        public static Guid GetRequestId()
+        {
+            // TODO: Pick one:
+            Guid result1;
+            Guid result2;
+            Guid result3;
+            try
+            {
+                result3 = _requestId;
+                result1 = (Guid)System.Web.HttpContext.Current.Items[REQUEST_ID_KEY];
+                result2 = (Guid)Thread.GetData(Thread.GetNamedDataSlot(REQUEST_ID_KEY));
+            }
+            catch (InvalidCastException)
+            {
+                return Guid.Empty;
+            }
+
+            if (result3.Equals(result2) && result3.Equals(result1))
+            {
+                return result3;
+            }
+            else
+            {
+                throw new InvalidOperationException("The three RequestIds don't match: " + result1.ToString() + " and " + result2.ToString() + " and " + result3.ToString());
+            }
+        }
+    }
+}
