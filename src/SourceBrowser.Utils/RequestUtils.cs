384afc2848ed31902bc1cd403c7c911ad871bb5f
Amadeus Wieczorek <git@amadeusw.com>
2015-01-15 15:09:25 -0800

    Removes redundant ways of persisting RequestId
    Settling on [ThreadStatic] attribute.
    ((Guid)null).ToString() returns 0000-00...

--- src/SourceBrowser.Utils/RequestUtils.cs
+++ src/SourceBrowser.Utils/RequestUtils.cs
@@ -5,8 +5,6 @@ namespace SourceBrowser.Utils
 {
     public class RequestUtils
     {
-        private const string REQUEST_ID_KEY = "RequestId";
-
         [ThreadStatic]
         private static Guid _requestId;
 
@@ -15,11 +13,7 @@ namespace SourceBrowser.Utils
         /// </summary>
         public static void CreateRequestId()
         {
-            Guid requestId = Guid.NewGuid();
-            // TODO: Pick one:
-            System.Web.HttpContext.Current.Items.Add(REQUEST_ID_KEY, requestId);
-            Thread.SetData(Thread.GetNamedDataSlot(REQUEST_ID_KEY), requestId);
-            _requestId = requestId;
+            _requestId = Guid.NewGuid();
         }
 
         /// <summary>
@@ -28,29 +22,7 @@ namespace SourceBrowser.Utils
         /// <returns>Guid associated with current HttpContext/Thread, or Guid.Empty if no Guid was found.</returns>
         public static Guid GetRequestId()
         {
-            // TODO: Pick one:
-            Guid result1;
-            Guid result2;
-            Guid result3;
-            try
-            {
-                result3 = _requestId;
-                result1 = (Guid)System.Web.HttpContext.Current.Items[REQUEST_ID_KEY];
-                result2 = (Guid)Thread.GetData(Thread.GetNamedDataSlot(REQUEST_ID_KEY));
-            }
-            catch (InvalidCastException)
-            {
-                return Guid.Empty;
-            }
-
-            if (result3.Equals(result2) && result3.Equals(result1))
-            {
-                return result3;
-            }
-            else
-            {
-                throw new InvalidOperationException("The three RequestIds don't match: " + result1.ToString() + " and " + result2.ToString() + " and " + result3.ToString());
-            }
+            return _requestId;
         }
     }
 }
